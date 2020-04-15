6987b8aac02d299357239d7c49563a8e2d8bb17b
jpd61 <jpd@dewoodyoil.com>
2020-04-15 15:46:06 -0500

    set daySchedule and localStorage retrieval

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 var dayFormat = moment().format("ddd, MMMM Do");
 
 var now = moment().format("H A");
+
+var daySchedule = [
+	{ time: "9 AM", event: "" },
+	{ time: "10 AM", event: "" },
+	{ time: "11 AM", event: "" },
+	{ time: "12 PM", event: "" },
+	{ time: "1 PM", event: "" },
+	{ time: "2 PM", event: "" },
+	{ time: "3 PM", event: "" },
+	{ time: "4 PM", event: "" },
+	{ time: "5 PM", event: "" }
+];
+
+var checkStorage = JSON.parse(localStorage.getItem("savedSchedule"));
+
